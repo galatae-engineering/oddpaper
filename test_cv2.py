c1ef3db00456977b03ff919e281af6db787cf3f1
Rasp pi4 B <elza@galatae.com>
2025-07-17 11:26:57 +0200

    reussite du test_cv2 pour detection des arucos. A noter que le programme n est pas lancable de johnny, il faut le faire a partir de cmd

--- test_cv2.py
+++ test_cv2.py
@@ -4,13 +4,28 @@ import numpy as np
 
 import os
 
+
 cam = cv2.VideoCapture(0)
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+parameters = cv2.aruco.DetectorParameters()
 
 while True:
     ret, frame = cam.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #detection d un marquer aruco appartenant a la bibliotheque definie
+    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+    
+    #recuperation du data de l aruco detecte
+    corners, ids, rejected = detector.detectMarkers(gray)
+    
+    print("Detected markers:", ids)
+    if ids is not None:
+        cv2.aruco.drawDetectedMarkers(frame,corners,ids)
     cv2.imshow('Camera',frame)
     
-    if cv2.waitkey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows()
