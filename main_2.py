1e9ee5e15f2974ec203da88fda23307246596463
Rasp pi4 B <elza@galatae.com>
2025-07-30 12:13:08 +0200

    Implementation du callage de papier ainsi que debut du unloading de la perforatrice. Bug persistant sur la fonction rob.get_tool_pose() dans la ligne 198 de main_2.py

--- main_2.py
+++ main_2.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import cv2
 import numpy as np
 import os
+import math
 import time
 import random as rand
 sys.path.append('../galatae-api/')
@@ -41,12 +42,17 @@ bpile3=0
 bpile4=0
 
 
-xperfo=400
+xperfo=350
 yperfo=0
 zperfo=150
 aperfo=110
 bperfo=0
 
+xtas1 = 150
+ytas1 = 250
+ztas = 300
+atas = apile
+bpile1 = 0
 l_xpile = [xpile13,xpile24,xpile13,xpile24]
 l_ypile = [ypile12,ypile12,ypile34,ypile34]
 l_bpile = [bpile1,bpile2,bpile3,bpile4]
@@ -66,7 +72,7 @@ GPIO.setup(Push_single,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 """  ###################  END SETUP   #################### """
 """  ############  USER CONFIGURATION MODE  ############## """
-n_cahier = 14    #number of pages in the book
+n_cahier = 2    #number of pages in the book
 print("")
 print("Choose if random or not")
 print("If random, push left, if single pile, push right")
@@ -76,12 +82,14 @@ while (b_random and b_single):
     b_random = GPIO.input(Push_random)
     b_single = GPIO.input(Push_single)
 """  ############  USER CONFIGURATION END   ############## """
-
+"""  ############  START LOOP SETUP  ########## """
 n_max = 10  ## nombre de feuilles maximales autorisées dans la perforatrice
 n_perfo = int(n_cahier/n_max) ## nombre de perforations pour un cahier complet
 n_reste = n_cahier%n_max ## nombre de feuilles restantes a perforer pour completer le cahier
 print("Nombre de perforations de paquets de 10:", n_perfo)
 print("Nombre de feuilles restantes:", n_reste)
+"""  ############  END LOOP SETUP  ########### """
+"""  ########### SART LOADING PERFO  ######### """
 for i_perfo in range(n_perfo+1):
     if i_perfo == n_perfo:
         n_loop = n_reste
@@ -91,41 +99,34 @@ for i_perfo in range(n_perfo+1):
         for n in range(n_loop):
             cam = cv2.VideoCapture(0)
             time.sleep(0.5)
-            """ Checking the wheter the piles are empty"""
             ret,frame = cam.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             #detection d un marquer aruco appartenant a la bibliotheque definie
             detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
             #recuperation du data de l aruco detecte
-            corners, ids, rejected = detector.detectMarkers(gray)
-            print("arucos detected: ",ids)   #ids is the identifiant of the piles that are empty
+            corners, ids, rejected = detector.detectMarkers(gray)  #ids is the identifiant of the piles that are empty
 
             if ids is not None:   
                 cv2.aruco.drawDetectedMarkers(frame,corners,ids)
+            else:
+                ids = []
             cv2.imshow('Camera',frame)
             
             cam.release()
             cv2.destroyAllWindows()
-            
-            """ Start feeding the perforatrice process"""
             if  len(ids)<pile_max :
                 print("")
-                print("Starting with paper n:",n + i_perfo*10)
-                print("brandom: ",b_random)
-                print("bnormal: ",b_single)
+                print("Starting with paper ",n +1 + i_perfo*10)
                 rand_pile = 0
                 if not b_random :
                     print("Random mode chosen")
                     rand_pile = rand.randint(0,pile_max-1)
-                    print("Pile selected : ",rand_pile)
                     while [rand_pile] in ids :
                         rand_pile = rand.randint(0,pile_max-1)
-                        print("Pile selected : ",rand_pile)
                 elif not b_single :
                     print("Normal mode chosen")
                     while [rand_pile] in ids :
                         rand_pile +=1
-                        print("Pile selected : ",rand_pile)
                 print("will go to pile",rand_pile+1)
                 xpile = l_xpile[rand_pile]
                 ypile = l_ypile[rand_pile]
@@ -156,7 +157,11 @@ for i_perfo in range(n_perfo+1):
                 print("probing for perforatrice")
                 r.set_joint_speed(probe_speed)
                 probe = r.probe([xperfo,yperfo,zperfo,aperfo,bperfo])
-
+                if probe == True :
+                    p_perfo = r.get_tool_pose()
+                    print("p_perfo: ",p_perfo)
+                else:
+                    p_perfo = [xperfo,yperfo,zperfo,aperfo,bperfo]
                 GPIO.output(Relais,GPIO.LOW)    ### Turn off the vacuun
                 time.sleep(3)
                 
@@ -166,7 +171,40 @@ for i_perfo in range(n_perfo+1):
             else:
                 print("zero paper left")
                 break
+        """ ############### END LOADING PERFO ############### """
+        """ ############### START OF PERFO ###############"""
+        time.sleep(0.5)
+        r.set_joint_speed(probe_speed)
+        r.go_to_point([p_perfo[0]-28,p_perfo[1]-150,p_perfo[2],p_perfo[3],p_perfo[4]])
+        time.sleep(2)
+        r.go_to_point([p_perfo[0]-28,p_perfo[1],p_perfo[2],p_perfo[3],p_perfo[4]])
+        time.sleep(2)
+        print("going in front of the perforatrice")
+        r.set_joint_speed(normal_speed)
+        r.go_to_point([xperfo-200,yperfo,zperfo,aperfo,bperfo])
+        time.sleep(4)
+        #### PUT HERE THE ACTIVATION OF THE PERFORATRICE
+        """ ############### END OF PERFO ##############"""
+        """ ############### SART UNLOADING ############"""
+        for i in range(n_loop):
+            print("probing for the perfo")
+            r.set_joint_speed(probe_speed)
+            probe = r.probe([xperfo,yperfo,zperfo,aperfo,bperfo])
+            if probe==True:
+                print("Starting vacuum")
+                GPIO.output(Relais,GPIO.HIGH)    ### Turn on the vacuum
+                time.sleep(5)
+            print("taking out the paper from the slot")
+            p_perfo = r.get_tool_pose()
+            r.go_to_point([p_perfo[0]+math.sin(math.radians(20))*60,p_perfo[1],p_perfo[2]+math.sin(math.radians(20))*60,p_perfo[3],p_perfo[4]])
+            time.sleep(1)
+            print("going in front of the perforatrice")
+            r.set_joint_speed(normal_speed)
+            r.go_to_point([xperfo-200,yperfo,zperfo,aperfo,bperfo])
+            
+        """ ############### END UNLOADING  ############"""
         print("returning to home position")
+        r.set_joint_speed(normal_speed)
         r.go_to_foetus_pos()
         r.set_joint_speed(normal_speed)
         r.reset_pos()
